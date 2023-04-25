c9bdda0b18c29fb95f20d827009be0a084094c4a
Renzo Arias <core.editor>
2023-04-25 01:53:25 -0500

    Limpiando App.js

--- src/App.js
+++ src/App.js
@@ -4,42 +4,15 @@ import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Filter from './components/Filter';
 import PostArchive from './components/PostArchive';
 import ProfilePage from './components/ProfilePage';
 import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 import ItemList from './components/Items';
 
-const auth = false;
+
 
 function App() {
 
-  if (!auth) {
-    return (
-      <Form className='mt-3'>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Usuario</Form.Label>
-        <Form.Control type="user" placeholder="Ingrese usuario" />
-        <Form.Text className="text-muted">
-
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-    )
-  }
-
- 
 return (
   <>
    
